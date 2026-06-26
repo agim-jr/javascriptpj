a2b7c74c77d212a852202b397595ac558fb14cd8
Junior Agim <junior.agim@gmail.com>
2026-06-26 15:37:14 -0400

    updated the index.js woth correct redirected url.

---
+++ index.js
@@ -0,0 +1,23 @@
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await response.json();
+    const userListEl = document.querySelector(".user-list");
+    console.log(usersData);
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+}
+main();
+
+function showUserPosts(id) {
+    window.location.href = `${window.location.origin}/user.html?id=${id}`;
+}
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.id})">
+        <div class="user-card__container">
+            <h3>${user.name}</h3>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+        </div>
+    </div>`;
+}
